14ed3158a18f3971520ed58dc1c08a364c99d263
lhl <edisonleolhl@foxmail.com>
2017-11-20 16:12:39 +0800

    some test and exercise code for chapter 2
    Signed-off-by: lhl <edisonleolhl@foxmail.com>

--- 第2章 提供推荐/deliciousrec.py
+++ 第2章 提供推荐/deliciousrec.py
@@ -1,15 +1,18 @@
-from pydelicious import get_popular,get_userposts,get_urlposts
 import time
+
+from pydelicious import get_popular, get_userposts, get_urlposts
+
 
 def initializeUserDict(tag,count=5):
   user_dict={}
   # get the top count' popular posts
   for p1 in get_popular(tag=tag)[0:count]:
-    # find all users who posted this
+    # find all users who posted this post
     for p2 in get_urlposts(p1['href']):
       user=p2['user']
       user_dict[user]={}
   return user_dict
+
 
 def fillItems(user_dict):
   all_items={}
